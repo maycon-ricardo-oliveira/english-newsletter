aa12ce5c1e4c5bc702a3e2b068041568cee71fff
Maycon Oliveira <rick.maycon.98@gmail.com>
2024-12-09 21:44:27 -0300

    Configura o GitHub Actions e estrutura do projeto

--- src/students.ts
+++ src/students.ts
@@ -4,11 +4,11 @@ const students = [
         email: "contato.maycon.ricardo@gmail.com",
         englishLevel: "A1"
     },
-    // {
-    //     name: "Rebeca Frizarin",
-    //     email: "rebecafrizarin@gmail.com",
-    //     englishLevel: "C2"
-    // }
+    {
+        name: "Rebeca Frizarin",
+        email: "rebecafrizarin@gmail.com",
+        englishLevel: "C2"
+    }
 ];
 
 export default students;
